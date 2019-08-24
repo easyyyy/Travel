886cd204d652b2baea9b95b02c5711a0d675965a
lin <849556032@qq.com>
2019-08-24 11:28:39 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+<!--    <input v-model="inputValue">-->
+<!--    <button @click="handleSubmit">提交</button>-->
+<!--    <ul>-->
+<!--&lt;!&ndash;      <li v-for="(item, index) of list" :key="index">{{item}}</li>&ndash;&gt;-->
+<!--      <todo-list v-for="(item, index) of list"-->
+<!--                 :key="index"-->
+<!--                 :item="item"-->
+<!--                 :index="index"-->
+<!--                 @delete="Delete"></todo-list>-->
+<!--    </ul>-->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>home</div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+
+</style>
