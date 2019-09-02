a5eaf3be23fab196a07cde5dcc22ecf9706c9ffe
lin <849556032@qq.com>
2019-09-02 10:36:35 +0800

    keep-alive

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <template>
   <div id="app">
-<!--    <input v-model="inputValue">-->
-<!--    <button @click="handleSubmit">提交</button>-->
-<!--    <ul>-->
-<!--&lt;!&ndash;      <li v-for="(item, index) of list" :key="index">{{item}}</li>&ndash;&gt;-->
-<!--      <todo-list v-for="(item, index) of list"-->
-<!--                 :key="index"-->
-<!--                 :item="item"-->
-<!--                 :index="index"-->
-<!--                 @delete="Delete"></todo-list>-->
-<!--    </ul>-->
-    <router-view></router-view>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
